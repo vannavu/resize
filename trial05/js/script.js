715a6f98ebad3af44b86785a446c65bcabcb106a
Vanna Vu <vannavu5539@gmail.com>
2018-11-14 15:07:24 -0600

    key press changing background :D

--- trial05/js/script.js
+++ trial05/js/script.js
@@ -3,16 +3,25 @@ $(document).ready(function() {
 	var on = false;
 	$('.box').click('click touchstart', function() {
 		if (on == false) {
-			$( this ).css( "opacity", "1" ),
+			$( this ).css("opacity", "1"),
 			on = true;
 		}
 		else {
-			$( this ).css( "opacity", "0.3" ),
+			$( this ).css( "opacity", "0.3"),
 			on = false;
 		}
 	});
 
-
-
-
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 69) {
+	    $(".box").css(
+				"background-image", "url(images/e_blue.svg)"
+	    );
+	  }
+		else if (keyPressed.keyCode == 79) {
+			$(".box").css(
+				"background", "url(images/o_blue.svg)");
+		}
+	});
+	
 });
