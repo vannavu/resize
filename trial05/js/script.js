307ac90eae47b63a658c1bc0b5b23b1171c7d88d
Vanna Vu <vannavu5539@gmail.com>
2018-11-19 23:58:59 -0600

    fixed transitions

--- trial05/js/script.js
+++ trial05/js/script.js
@@ -17,15 +17,24 @@ $(document).ready(function() {
 	    $(".box").css(
 				"background-image", "url(images/a_blue.svg)"
 	    );
+			$(".display").css(
+				"background", "url(images/a_blue.svg)"
+	    );
 	  }
 		else if (keyPressed.keyCode == 69) {
 	    $(".box").css(
 				"background-image", "url(images/e_blue.svg)"
 	    );
+			$(".display").css(
+				"background", "url(images/e_blue.svg)"
+	    );
 	  }
 		else if (keyPressed.keyCode == 79) {
 			$(".box").css(
 				"background", "url(images/o_blue.svg)");
+			$(".display").css(
+				"background", "url(images/o_blue.svg)"
+	    );
 		}
 	});
 
