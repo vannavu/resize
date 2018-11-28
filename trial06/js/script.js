0afe89afd55292d0feec21830a8d70e7bdda9af5
Vanna Vu <vannavu5539@gmail.com>
2018-11-28 11:08:57 -0600

    WOOOOOOO nav done, color 0.1, intro in prog

--- trial06/js/script.js
+++ trial06/js/script.js
@@ -1,67 +1,107 @@
 $(document).ready(function() {
 
-	var scheme = Math.random() * 5;
-  if (0 < scheme && scheme <=1) {
-    $(".box").css({
-      'filter': 'hue-rotate(0deg)'
-    });
-		$(".container, body").css({
-			'background': 'red'
-		});
-  }
-  else if (1 < scheme && scheme <= 2) {
-    $(".box").css({
-			'filter': 'grayscale(100%) brightness(10)'
-    });
-		$(".container, body").css({
-			'background': 'black'
-		});
-  }
-  else if (2 < scheme && scheme <= 3) {
-    $(".box").css({
-			'filter': 'hue-rotate(70deg) brightness(2.25)'
-    });
-		$(".container, body").css({
-			'background': '#266AFF'
-		});
-  }
-  else if (3 < scheme && scheme <= 4) {
-		$(".box").css({
-			'filter': 'hue-rotate(90deg) brightness(2.25)'
-    });
-		$(".container, body").css({
-			'background': 'rgba(0,0,0,0.1)'
-		});
-  }
-  else {
-		$(".box").css({
-			'filter': 'grayscale(100%) brightness(0)'
-		});
-		$(".container, body").css({
-			'background': 'yellow'
-		});
-  }
-
-	var on = false;
+	var start = false;
 	$('.box').click('click touchstart', function() {
-		if (on == false) {
-			$( this ).css("opacity", "1"),
-			on = true;
+		if (start == false) {
+			$(".box").css("opacity", "1");
+			start = true;
 		}
 		else {
-			$( this ).css( "opacity", "0.3"),
-			on = false;
+			if ($(window).width() > 800) {
+				$( this ).css("opacity", "0.5");
+			}
+			else {
+				$( this ).css("opacity", "1");
+			}
 		}
 	});
 
-	var activated = false;
+	var scheme = 1
 	$(document).keydown(function(keyPressed) {
-		if (keyPressed.keyCode == 65) {
-			activated = true;
+		if (keyPressed.keyCode == 32) {
+			scheme = scheme + 1;
+		  if (scheme == 1) {
+		    $(".box").css({
+		      'filter': 'hue-rotate(0deg)'
+		    });
+				$(".container, body").css({
+					'background': 'red'
+				});
+				$(".text").css({
+					'background': 'white',
+					'color': 'blue'
+				});
+				$(".nav").css({
+					'color': 'white'
+				});
+		  }
+		  else if (scheme == 2) {
+		    $(".box").css({
+					'filter': 'grayscale(100%) brightness(10)'
+		    });
+				$(".container, body").css({
+					'background': 'black'
+				});
+				$(".text").css({
+					'background': 'blue',
+					'color': 'white'
+				});
+				$(".nav").css({
+					'color': 'blue'
+				});
+		  }
+		  else if (scheme == 3) {
+		    $(".box").css({
+					'filter': 'hue-rotate(70deg) brightness(2.5)'
+		    });
+				$(".container, body").css({
+					'background': '#006DFF'
+				});
+				$(".text").css({
+					'background': 'lime',
+					'color': 'navy'
+				});
+				$(".nav").css({
+					'color': 'lime'
+				});
+		  }
+		  else if (scheme == 4) {
+				$(".box").css({
+					'filter': 'hue-rotate(90deg) brightness(2.25)'
+		    });
+				$(".container, body").css({
+					'background': 'rgba(0,0,0,0.1)'
+				});
+				$(".text").css({
+					'background': 'black',
+					'color': '#FF0054'
+				});
+				$(".nav").css({
+					'color': 'black'
+				});
+		  }
+		  else {
+				$(".box").css({
+					'filter': 'grayscale(100%) brightness(0)'
+				});
+				$(".container, body").css({
+					'background': 'yellow'
+				});
+				$(".text").css({
+					'background': '#D100FF',
+					'color': 'black'
+				});
+				$(".nav").css({
+					'color': '#D100FF'
+				});
+				scheme = 0;
+		  }
+
+	  }
+		else if (keyPressed.keyCode == 65) {
 			$("#welcome").css("display", "none");
 	    $(".box").css({
 				"background-image": "url(images/a_blue.svg)",
-				"cursor": "pointer"
 	    });
 			$(".display").css({
 				"background": "url(images/a_blue.svg)",
@@ -73,11 +113,9 @@ $(document).ready(function() {
   		});
 	  }
 		else if (keyPressed.keyCode == 69) {
-			activated = true;
 			$("#welcome").css("display", "none");
 	    $(".box").css({
 				"background-image": "url(images/e_blue.svg)",
-				"cursor": "pointer"
 	    });
 			$(".display").css({
 				"background": "url(images/e_blue.svg)",
@@ -89,11 +127,9 @@ $(document).ready(function() {
   		});
 	  }
 		else if (keyPressed.keyCode == 73) {
-			activated = true;
 			$("#welcome").css("display", "none");
 	    $(".box").css({
 				"background-image": "url(images/i_blue.svg)",
-				"cursor": "pointer"
 	    });
 			$(".display").css({
 				"background": "url(images/i_blue.svg)",
@@ -105,11 +141,9 @@ $(document).ready(function() {
   		});
 	  }
 		else if (keyPressed.keyCode == 79) {
-			activated = true;
 			$("#welcome").css("display", "none");
 			$(".box").css({
 				"background-image": "url(images/o_blue.svg)",
-				"cursor": "pointer"
 	    });
 			$(".display").css({
 				"background": "url(images/o_blue.svg)",
@@ -121,11 +155,9 @@ $(document).ready(function() {
   		});
 		}
 		else if (keyPressed.keyCode == 85) {
-			activated = true;
 			$("#welcome").css("display", "none");
 			$(".box").css({
 				"background-image": "url(images/u_blue.svg)",
-				"cursor": "pointer"
 	    });
 			$(".display").css({
 				"background": "url(images/u_blue.svg)",
@@ -135,11 +167,6 @@ $(document).ready(function() {
     		opacity: 0,
   		}, 1000, function() {
   		});
-		}
-		else {
-			if (activated == false) {
-				$("#welcome").css("display", "block");
-			}
 		}
 	});
 
